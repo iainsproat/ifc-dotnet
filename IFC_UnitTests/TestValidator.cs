057b504e8acf85e8578d236d0cad212fca54573d
iainsproat <iainsproat@8eeb4896-fc24-bb46-98b6-af5a22f29000>
2010-11-14 18:08:52 +0000

    Initial commit

---
+++ IFC_UnitTests/TestValidator.cs
@@ -0,0 +1,72 @@
+﻿/*
+
+Copyright 2010, Iain Sproat
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+    * The names of the contributors may not be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,           
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+using System;
+using NUnit.Framework;
+
+using IfcDotNet;
+
+using log4net;
+using log4net.Config;
+
+namespace IfcDotNet_UnitTests
+{
+    [TestFixture]
+    public class TestValidator
+    {
+        IfcXmlValidator validator;
+                
+        [Test]
+        public void CanValidate()
+        {
+            validator = new IfcXmlValidator( Utilities.getMinimumExample() );
+            Assert.IsTrue( validator.IsValid );
+            Assert.AreEqual(0, validator.Messages.Count);
+        }
+        
+        [Test]
+        public void CanValidateAlternative()
+        {
+        	validator = new IfcXmlValidator( Utilities.getAlternativeMinimumExample() );
+        	Assert.IsTrue( validator.IsValid );
+        	Assert.AreEqual( 0, validator.Messages.Count );
+        }
+        
+        [Test]
+        public void CanDetectInvalid()
+        {
+            validator = new IfcXmlValidator( Utilities.getFailingMinimumExample() );
+            Assert.IsFalse( validator.IsValid );
+            Assert.AreEqual(1, validator.Messages.Count);
+        }
+    }
+}

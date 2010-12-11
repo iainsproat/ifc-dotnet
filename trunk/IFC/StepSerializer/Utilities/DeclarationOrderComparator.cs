2d232e3541ef90c68de6ab51ffee11501e672ec5
iainsproat <iainsproat@8eeb4896-fc24-bb46-98b6-af5a22f29000>
2010-12-11 10:04:14 +0000

    More progress on the IfcStepSerializer.  IfcStepSerializer now creates an instance of the entity, and caches the properties.  It does not yet work, and the test still fails (the test is incomplete).

---
+++ trunk/IFC/StepSerializer/Utilities/DeclarationOrderComparator.cs
@@ -0,0 +1,60 @@
+﻿#region License
+/*
+
+Copyright 2010, Iain Sproat
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+ * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+ * The names of the contributors may not be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+#endregion
+using System;
+using System.Collections;
+using System.Reflection;
+
+namespace IfcDotNet.StepSerializer.Utilities
+{
+    /// <summary>
+    /// DeclarationOrderComparator tries to order PropertyInfos by their MetadataToken value
+    /// </summary>
+    public class DeclarationOrderComparator : IComparer
+    {
+        int IComparer.Compare(Object x, Object y)
+        {
+            PropertyInfo first = x as PropertyInfo;
+            PropertyInfo second = y as PropertyInfo;
+            if(first == null || second == null)
+                return 0;
+            
+            if (first.MetadataToken < second.MetadataToken)
+                return -1;
+            else if (first.MetadataToken > second.MetadataToken)
+                return 1;
+
+            return 0;
+        }
+    }
+}

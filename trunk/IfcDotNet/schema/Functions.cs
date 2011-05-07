09e2c47d2e4bdaf80ce1c76a97feaaceeda8a2da
iainsproat <iainsproat@8eeb4896-fc24-bb46-98b6-af5a22f29000>
2011-05-07 11:37:53 +0000

    Equality operators and hashcode to doublewrapper Implemented IfcCrossProduct - issue #11 Implemented IfcFirstProjAxis - issue #17 Implemented IfcScalarTimesVector - issue #32 Implemented IfcSecondProjAxis - issue #33 Implemented IfcVectorDifference - issue #41

--- trunk/IfcDotNet/schema/Functions.cs
+++ trunk/IfcDotNet/schema/Functions.cs
@@ -81,7 +81,7 @@ namespace IfcDotNet.Schema
 			
 			IList<IfcDirection> U = new List<IfcDirection>(Dim);
 			double Factor;
-			IfcDirection D1, D2;
+			IfcDirection D1;
 			
 			if(axis1 != null){
 				D1 = IfcNormalise(axis1);
@@ -140,6 +140,35 @@ namespace IfcDotNet.Schema
 			return axes;
 		}
 		
+		/// <summary>
+		/// This function returns the vector (or cross) product of two input directions. 
+		/// The input directions must be three-dimensional. 
+		/// The result is always a vector which is unitless.
+		/// If the input directions are either parallel or anti-parallel a vector of zero magnitude is returned.
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		/// <returns></returns>
+		public static IfcVector IfcCrossProduct(IfcDirection arg1, IfcDirection arg2){
+		    if(arg1 == null || arg1.Dim != 3 || arg2 == null || arg2.Dim != 3)
+		        return null;
+		    
+		    doublewrapper Mag;
+		    IfcDirection Res;
+		    IList<doublewrapper> V1, V2;
+		    
+		    V1 = new List<doublewrapper>(IfcNormalise(arg1).DirectionRatios.doublewrapper);
+		    V2 = new List<doublewrapper>(IfcNormalise(arg2).DirectionRatios.doublewrapper);
+		    Res = new IfcDirection(V1[1]*V2[2] - V1[2]*V2[1], V1[2]*V2[0] - V1[0]*V2[2], V1[0]*V2[1] - V1[1]*V2[0]);
+		    Mag = 0;
+		    for(int i = 0; i < 3; i++){
+		        Mag += Res.DirectionRatios[i]*Res.DirectionRatios[i];
+		    }
+		    if(Mag > 0)
+		        return new IfcVector(Res, Math.Sqrt((double)Mag));
+		    else
+		        return new IfcVector(arg1, 0);
+		}
 		
 		/// <summary>
 		/// Definition from ISO/CD 10303-41:1992: The function returns the dimensional exponents of the given SI-unit.
@@ -227,6 +256,42 @@ namespace IfcDotNet.Schema
 		}
 		
 		/// <summary>
+		/// This function produces a three dimensional direction which is, with fully defined input,
+		/// the projection of arg onto the plane normal to the z-axis.
+		/// With arg defaulted the result is the projection of (1.0,0.0,0.0) onto this plane
+		/// except that if z-axis = (1.0,0.0,0.0) then (0.0,1.0,0.0) is used as initial value of arg
+		/// A violation occurs if arg is in the same direction as the input z-axis.
+		/// </summary>
+		/// <param name="zAxis"></param>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		public static IfcDirection IfcFirstProjAxis(IfcDirection zAxis, IfcDirection arg){
+			if(zAxis == null) return null;
+			
+			IfcDirection xAxis, V, Z;
+			IfcVector xVec;
+			
+			Z = IfcNormalise(zAxis);
+			if(arg == null){
+				if(Z.DirectionRatios[0] != 1 || Z.DirectionRatios[1] != 0 || Z.DirectionRatios[2] != 0){
+					V = new IfcDirection(1,0,0);
+				}else{
+					V = new IfcDirection(0,1,0);
+				}
+			}else{
+				if(arg.Dim != 3) return null;
+				if(IfcCrossProduct(arg, Z).Magnitude == 0)
+					return null;
+				else
+					V = IfcNormalise(arg);
+			}
+			
+			xVec = IfcScalarTimesVector(IfcDotProduct(V, Z), Z);
+			xAxis = IfcVectorDifference(V, xVec).Orientation.Item;
+			return IfcNormalise(xAxis);
+		}
+		
+		/// <summary>
 		/// This function returns a vector whose components are normalized to have a sum of squares of 1.0.
 		/// If the input argument is not defined or of zero length then null is returned.
 		/// </summary>
@@ -305,6 +370,180 @@ namespace IfcDotNet.Schema
 			if(Vec == null || Vec.Dim != 2)
 				return null;
 			return new IfcDirection(-(double)Vec.DirectionRatios[1], (double)Vec.DirectionRatios[0]);
+		}
+		
+		/// <summary>
+		/// This function returns the vector that is the scalar multiple of the input vector. 
+		/// It accepts as input a scalar and a 'vector' which may be either a Direction or a Vector. 
+		/// The output is a Vector of the same units as the input vector or unitless if a direction is input.
+		/// If either input argument is undefined then the returned vector is also undefined.
+		/// </summary>
+		/// <param name="scalar"></param>
+		/// <param name="vec"></param>
+		/// <returns></returns>
+		public static IfcVector IfcScalarTimesVector(double scalar, IfcDirection vec){
+		    if(vec == null) return null;
+		    
+		    IfcDirection V;
+		    double Mag;
+		    
+		    V = vec;//FIXME clone?
+		    Mag = scalar;
+		    
+		    if(Mag < 0){
+		        for(int i = 0; i < V.DirectionRatios.doublewrapper.Length; i++){
+		            V.DirectionRatios[i] = -V.DirectionRatios[i];
+		        }
+		        Mag = -Mag;
+		    }
+		    return new IfcVector(IfcNormalise(V), Mag);
+		}
+		
+		/// <summary>
+		/// This function returns the vector that is the scalar multiple of the input vector. 
+		/// It accepts as input a scalar and a 'vector' which may be either a Direction or a Vector. 
+		/// The output is a Vector of the same units as the input vector or unitless if a direction is input.
+		/// If either input argument is undefined then the returned vector is also undefined.
+		/// </summary>
+		/// <param name="scalar"></param>
+		/// <param name="vec"></param>
+		/// <returns></returns>
+		public static IfcVector IfcScalarTimesVector(double scalar, IfcVector vec){
+		    if(vec == null) return null;
+		    
+		    IfcDirection V;
+		    double Mag;
+		    
+		    V = vec.Orientation.Item;
+		    Mag = scalar * vec.Magnitude;
+		    
+		    if(Mag < 0){
+		        for(int i = 0; i < V.DirectionRatios.doublewrapper.Length; i++){
+		            V.DirectionRatios[i] = -V.DirectionRatios[i];
+		        }
+		        Mag = -Mag;
+		    }
+		    return new IfcVector(IfcNormalise(V), Mag);
+		}
+		
+		/// <summary>
+		/// This function returns the normalized vector
+		/// that is simultaneously the projection of arg onto the plane normal to the vector z-axis
+		/// and onto the plane normal to the vector x-axis.
+		/// If arg is NULL then the projection of the vector (0.,1.,0.) onto z-axis is returned.
+		/// </summary>
+		/// <param name="zAxis"></param>
+		/// <param name="xAxis"></param>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		public static IfcDirection IfcSecondProjAxis(IfcDirection zAxis, IfcDirection xAxis, IfcDirection arg){
+			IfcVector yAxis, temp;
+			IfcDirection V;
+			
+			if(arg == null)
+				V = new IfcDirection(0, 1, 0);
+			else
+				V = arg;
+			
+			temp  = IfcScalarTimesVector(IfcDotProduct(V, zAxis), zAxis);
+			yAxis = IfcVectorDifference(V, temp);
+			temp  = IfcScalarTimesVector(IfcDotProduct(V, xAxis), xAxis);
+			yAxis = IfcVectorDifference(yAxis, temp);
+			yAxis = IfcNormalise(yAxis);
+			return yAxis.Orientation.Item;			
+		}
+		
+		/// <summary>
+		/// This function returns the difference of the input arguments as (Arg1 - Arg2). 
+		/// The function returns as a vector the vector difference of the two input vectors. 
+		/// The input arguments shall both be of the same dimensionality but may be either directions or vectors. 
+		/// If both input arguments are vectors they must be expressed in the same units, if both are directions a unitless result is produced. 
+		/// A zero difference vector produces a vector of zero magnitude.
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		/// <returns></returns>
+		public static IfcVector IfcVectorDifference(IfcDirection arg1, IfcDirection arg2){
+		    if(arg1 == null || arg2 == null || arg1.Dim != arg2.Dim)
+		        return null;
+		    
+		    return IfcVectorDifference(1, arg1, 1, arg2);
+		}
+		
+		/// <summary>
+		/// This function returns the difference of the input arguments as (Arg1 - Arg2). 
+		/// The function returns as a vector the vector difference of the two input vectors. 
+		/// The input arguments shall both be of the same dimensionality but may be either directions or vectors. 
+		/// If both input arguments are vectors they must be expressed in the same units, if both are directions a unitless result is produced. 
+		/// A zero difference vector produces a vector of zero magnitude.
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		/// <returns></returns>
+		public static IfcVector IfcVectorDifference(IfcVector arg1, IfcVector arg2){
+		    if(arg1 == null || arg2 == null || arg1.Dim != arg2.Dim)
+		        return null;
+		    return IfcVectorDifference(arg1.Magnitude, arg1.Orientation.Item, arg2.Magnitude, arg2.Orientation.Item);
+		}
+		
+		/// <summary>
+		/// This function returns the difference of the input arguments as (Arg1 - Arg2). 
+		/// The function returns as a vector the vector difference of the two input vectors. 
+		/// The input arguments shall both be of the same dimensionality but may be either directions or vectors. 
+		/// If both input arguments are vectors they must be expressed in the same units, if both are directions a unitless result is produced. 
+		/// A zero difference vector produces a vector of zero magnitude.
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		/// <returns></returns>
+		public static IfcVector IfcVectorDifference(IfcVector arg1, IfcDirection arg2){
+		    if(arg1 == null || arg2 == null || arg1.Dim != arg2.Dim)
+		        return null;
+		    return IfcVectorDifference(arg1.Magnitude, arg1.Orientation.Item, 1, arg2);		
+		}
+		
+		/// <summary>
+		/// This function returns the difference of the input arguments as (Arg1 - Arg2). 
+		/// The function returns as a vector the vector difference of the two input vectors. 
+		/// The input arguments shall both be of the same dimensionality but may be either directions or vectors. 
+		/// If both input arguments are vectors they must be expressed in the same units, if both are directions a unitless result is produced. 
+		/// A zero difference vector produces a vector of zero magnitude.
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		/// <returns></returns>
+		public static IfcVector IfcVectorDifference(IfcDirection arg1, IfcVector arg2){
+		    if(arg1 == null || arg2 == null || arg1.Dim != arg2.Dim)
+		        return null;
+		    return IfcVectorDifference(1, arg1, arg2.Magnitude, arg2.Orientation.Item);		
+		}
+		
+		private static IfcVector IfcVectorDifference(double Mag1, IfcDirection Vec1, double Mag2, IfcDirection Vec2){
+		    IfcDirection Res;
+		    doublewrapper Mag;
+		    int nDim;
+		    
+		    Vec1 = IfcNormalise(Vec1);
+		    Vec2 = IfcNormalise(Vec2);
+		    nDim = Vec1.DirectionRatios.doublewrapper.Length;
+		    Mag = 0;
+		    Res = new IfcDirection();
+		    doublewrapper[] temp = new doublewrapper[nDim];
+		    for(int i = 0; i < nDim; i++){
+		        temp[i] = 0;
+		    }
+		    Res.DirectionRatios = temp;
+		    
+		    for(int i = 0; i < nDim; i++){
+		        Res.DirectionRatios[i] = Mag1*Vec1.DirectionRatios[i] - Mag2*Vec2.DirectionRatios[i];
+		        Mag += Res.DirectionRatios[i]*Res.DirectionRatios[i];
+		    }
+		    
+		    if(Mag > 0){
+		        return new IfcVector(Res, Math.Sqrt((double)Mag));
+		    }else{
+		        return new IfcVector(Vec1, 0);
+		    }
 		}
 	}
 }
